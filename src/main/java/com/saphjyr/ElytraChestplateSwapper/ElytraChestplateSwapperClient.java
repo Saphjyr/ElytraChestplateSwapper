38c541e840daaf2d54154d26902e7d78f2ad3189
saphjyr <nan@nan.nan>
2023-03-04 11:04:09 +0100

    FabricElytraItem compatibility

--- src/main/java/com/saphjyr/ElytraChestplateSwapper/ElytraChestplateSwapperClient.java
+++ src/main/java/com/saphjyr/ElytraChestplateSwapper/ElytraChestplateSwapperClient.java
@@ -3,18 +3,20 @@ package com.saphjyr.ElytraChestplateSwapper;
 import org.lwjgl.glfw.GLFW;
 
 import net.fabricmc.api.ClientModInitializer;
-
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
 public class ElytraChestplateSwapperClient implements ClientModInitializer {
     
     public static SwapKeyBinding keyBinding;
+    
     @Override
     public void onInitializeClient() {
 
         // Create and log the key binding
         keyBinding = new SwapKeyBinding("key.ecs.swap", GLFW.GLFW_KEY_GRAVE_ACCENT, "category.ecs");
         KeyBindingHelper.registerKeyBinding(keyBinding);
+
+        
         
     }
 
--- src/main/java/com/saphjyr/ElytraChestplateSwapper/InventoryUtils.java
+++ src/main/java/com/saphjyr/ElytraChestplateSwapper/InventoryUtils.java
@@ -1,70 +1,124 @@
 package com.saphjyr.ElytraChestplateSwapper;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ElytraItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.SlotActionType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.entity.event.v1.FabricElytraItem;
 
 public class InventoryUtils {
+
+    @Environment(net.fabricmc.api.EnvType.CLIENT)
     public static void swapChestplate(MinecraftClient client){
+
+        // Check if client.player is not null
+        if (client.player == null) return;
+
+        // Check if it is a player
+        if (!(client.player instanceof ClientPlayerEntity)) return;
+
+        // Check if is it not dead
+        if (client.player.isDead()) return;
 
         // Slots to swap to
         int elytraSlot = -1;
         int chestplateSlot = -1;
 
-        // List inventory slots, in a special order so the selected slot is the most top-left chestplate
-        int[] range = new int[37];
-        for (int i = 0; i<9; i++) range[i] = 8-i;
-        for (int i = 9; i<36; i++) range[i] = 35-(i-9);
-        range[36] = 40; // Off hand
+        int HOTBAR_SIZE = PlayerInventory.getHotbarSize(); // 9
+        int MAIN_SIZE = PlayerInventory.MAIN_SIZE; // 36
+        int TOTAL_SIZE = MAIN_SIZE + 1; // 37
+
+        // List inventory slots, in a special order so the selected slot is the most top-left chestplate : 9 - 35 40 0 - 8
+        int[] range = new int[TOTAL_SIZE];
+
+        // Main inventory
+        for (int i = 0; i < MAIN_SIZE - HOTBAR_SIZE; i++) {
+            range[i] = i + HOTBAR_SIZE;
+        }
+
+        // Off hand
+        range[MAIN_SIZE - HOTBAR_SIZE] = PlayerInventory.OFF_HAND_SLOT; // 40
+
+        // Hotbar
+        for (int i = 0; i < HOTBAR_SIZE; i++) {
+            range[i + MAIN_SIZE - HOTBAR_SIZE + 1] = i;
+        }
 
         // Find elytraSlot and chestplateSlot
         for(int slot : range) {
 
+            // Get the itemstack in the slot
             ItemStack stack = client.player.getInventory().getStack(slot);
-            
-            if(stack.isEmpty() || !(stack.getItem() instanceof ArmorItem || stack.getItem() instanceof ElytraItem))
+
+            // Check if the itemstack is empty
+            if (stack.isEmpty()) {
                 continue;
-            if (stack.getItem() instanceof ElytraItem) {
-                elytraSlot = slot;
-                continue;
+            } 
+            else {
+
+                // Check if the item is an elytra item
+                if (isElytra(stack) && elytraSlot < 0) {
+                    elytraSlot = slot;
+                }
+
+                // Check if the item is a chestplate item
+                else if (isChestplate(stack) && chestplateSlot < 0) {
+                    chestplateSlot = slot;
+                }
             }
 
-            ArmorItem armorItem = (ArmorItem)stack.getItem();
-            if (armorItem.getSlotType() == EquipmentSlot.CHEST) {
-                chestplateSlot = slot;
-            }
+
         }
 
-        // Swap with slot depending on currently weard item
+        // Get the itemstack in the chestplate slot
         ItemStack wearedItemStack =  client.player.getInventory().getStack(38);
         if (wearedItemStack.isEmpty() && elytraSlot >= 0) {
-            swap(elytraSlot, client);
+            sendSwapPackets(elytraSlot, client);
         }
-        else if (wearedItemStack.getItem() instanceof ElytraItem && chestplateSlot >=0) {
-            swap(chestplateSlot, client);
+        else if (isElytra(wearedItemStack) && chestplateSlot >=0) {
+            sendSwapPackets(chestplateSlot, client);
         }
-        else if (wearedItemStack.getItem() instanceof ArmorItem && elytraSlot >=0) {
-            swap(elytraSlot, client);
+        else if (isChestplate(wearedItemStack) && elytraSlot >=0) {
+            sendSwapPackets(elytraSlot, client);
         }
         
     }
 
-    private static void swap(int slot, MinecraftClient client) {
-        int slot2 = slot;
-        if (slot2 == 40) slot2 = 45; // Off Hand offset
-        if(slot2 < 9) slot2 += 36;   // Toolbar offset
+    private static boolean isElytra(ItemStack stack) {
+        return stack.getItem() instanceof ElytraItem || stack.getItem() instanceof FabricElytraItem;
+    }
+
+    private static boolean isChestplate(ItemStack stack) {
+        boolean isChestplate = false;
+        if (stack.getItem() instanceof ArmorItem) {
+            ArmorItem armorItem = (ArmorItem)stack.getItem();
+            if (armorItem.getSlotType() == EquipmentSlot.CHEST) {
+                isChestplate = true;
+            }
+        }
+        return isChestplate;
+    }
+
+    private static void sendSwapPackets(int slot, MinecraftClient client) {
+        int sentSlot = slot;
+        if (sentSlot == PlayerInventory.OFF_HAND_SLOT) sentSlot += 5; // Off Hand offset
+        if(sentSlot < PlayerInventory.getHotbarSize()) sentSlot += PlayerInventory.MAIN_SIZE;   // Toolbar offset
         
 
         // Take the item to swap to
-        client.interactionManager.clickSlot(0, slot2, 0, SlotActionType.PICKUP, client.player);
+        client.interactionManager.clickSlot(0, sentSlot, 0, SlotActionType.PICKUP, client.player);
 
         // Put it in the armor slot
         client.interactionManager.clickSlot(0, 6, 0, SlotActionType.PICKUP, client.player);
 
         // Put back what was in the armor slot (can be air)
-        client.interactionManager.clickSlot(0, slot2, 0, SlotActionType.PICKUP, client.player);
+        client.interactionManager.clickSlot(0, sentSlot, 0, SlotActionType.PICKUP, client.player);
     }
+
+
 }
